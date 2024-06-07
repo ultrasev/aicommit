82eb667184ef982c5e9e4593152ce40767d0561e
ultrasev <i@ultrasev.com>
2024-06-08 01:07:55 +0800

    fix: correct response mapping for chat completions

--- aicommit/commit.py
+++ aicommit/commit.py
@@ -47,14 +47,15 @@ class CommitGenerator(object):
         self.client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
     def __str__(self) -> str:
-        return self.client.chat.completions.create(
+        resp = self.client.chat.completions.create(
             model='gpt-3.5-turbo',
             messages=[
                 {'role': 'assistant', 'content': 'You are a helpful assistant.'},
                 {'role': 'user', 'content': PROMPT_TEMPLATE.format(
                     self.diff)},
             ]
-        )['choices'][0]['message']['content']
+        )
+        return resp.choices[0].message.content
 
 
 class NoChangesException(Exception):
